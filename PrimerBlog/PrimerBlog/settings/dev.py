43d6a2959228307c4a08742b1bd80384b8d7bd5a
Francisco Riofrio <geovanny07121985@gmail.com>
2022-11-01 00:12:01 -0500

    Subir Proyecto

---
+++ PrimerBlog/PrimerBlog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-k%93w1ng4*8q1c@vc)os@pkga*j4tj1ptk-hfbp1*r6$9f)=qs"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
